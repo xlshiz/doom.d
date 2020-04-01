ac1a51a9fb11390817fd8a6a72f75dbce6018bb3
shixl <xlshiz@users.noreply.github.com>
2020-04-01 22:16:02 +0800

    Sync

--- packages.el
+++ packages.el
@@ -4,6 +4,6 @@
 (package! edit-indirect)
 (package! paradox)
 (package! imenu-list)
-
+(package! nox :recipe (:host github :repo "manateelazycat/nox") :pin "6e6e799d39")
 
 (disable-packages! doom-snippets)
