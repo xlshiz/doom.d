e17c4a9039116b12414f4336f4cbf0cd5adbc052
shixl <xlshiz@users.noreply.github.com>
2020-04-03 22:46:48 +0800

    Sync

--- modules/private/intl/packages.el
+++ modules/private/intl/packages.el
@@ -8,6 +8,6 @@
 (package! insert-translated-name :recipe (:host github :repo "manateelazycat/insert-translated-name"))
 (package! pinyinlib.el :recipe (:host github :repo "xlshiz/pinyinlib.el"))
 (package! pangu-spacing)
-(package! pyim :pin "84af8e80a4")
+(package! pyim :pin "8fe9897f57")
 (when (featurep! +rime)
   (package! liberime :pin "8c84d5daa2" :recipe (:host github :repo "merrickluo/liberime" :files ("CMakeLists.txt" "Makefile" "src" "liberime*.el" "liberime.el"))))
