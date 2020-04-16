a0ed5492d3438ec5a868c2569b637ec0bfe4bd85
shixl <xlshiz@users.noreply.github.com>
2020-04-16 12:21:46 +0800

    Sync

--- config.el
+++ config.el
@@ -78,6 +78,7 @@
 
 (after! lsp-mode
   (setq lsp-enable-symbol-highlighting nil
+	lsp-vetur-validation-template nil
         lsp-eldoc-enable-hover t))
 
 (after! lsp-ui
--- modules/private/lsp/packages.el
+++ modules/private/lsp/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/tabbar/packages.el
 
-(package! nox :recipe (:host github :repo "manateelazycat/nox") :pin "6e6e799d39")
+(package! nox :recipe (:host github :repo "manateelazycat/nox") :pin "f582534bf7")
