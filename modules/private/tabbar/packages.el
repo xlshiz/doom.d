bc51f386fa3d3c72fdd9ec85810cdfc73fa85a29
shixl <xlshiz@users.noreply.github.com>
2020-08-24 06:27:39 +0800

    Sync

--- modules/private/tabbar/packages.el
+++ modules/private/tabbar/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/tabbar/packages.el
 
-(package! awesome-tab :recipe (:host github :repo "manateelazycat/awesome-tab" :files ("*.el") :no-byte-compile t) :pin "dd8ce9be886ad4e410a8589551039bfe940dffce")
+(package! awesome-tab :recipe (:host github :repo "manateelazycat/awesome-tab" :files ("*.el") :no-byte-compile t) :pin "053b1f3d2b70405a1addb3a9f5ca6f70fa26a19c")
