1b13678ca46baa8dbf3b158a405d22d8cb912073
shixl <xlshiz@users.noreply.github.com>
2020-10-16 06:46:18 +0800

    Sync

--- modules/private/lsp/packages.el
+++ modules/private/lsp/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; private/tabbar/packages.el
 
-(package! nox :recipe (:host github :repo "manateelazycat/nox" :no-byte-compile t) :pin "1293812f9c")
+(package! nox :recipe (:host github :repo "manateelazycat/nox" :no-byte-compile t) :pin "22bd62fd6f6dad633d17834d5e4d56d1855b2c53")
