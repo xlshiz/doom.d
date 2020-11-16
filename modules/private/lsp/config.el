83ee0da203cd93d7bdbc2bd2a9914028398768af
shixl <xlshiz@users.noreply.github.com>
2020-11-16 22:08:10 +0800

    Sync

--- modules/private/lsp/config.el
+++ modules/private/lsp/config.el
@@ -7,6 +7,8 @@
   (defalias 'lsp-rename #'nox-rename)
   (setq nox-optimization-p nil)
   :config
+  (setq nox-autoshutdown t)
+
   ;; js-mode
   (delete '((js-mode typescript-mode) . ("javascript-typescript-stdio")) nox-server-programs)
   (add-to-list 'nox-server-programs '((js-mode typescript-mode) . ("typescript-language-server" "--stdio")))
@@ -32,6 +34,7 @@
 
   ;; python-mode
   (setq nox-python-server-dir (concat doom-etc-dir "lsp/mspyls/"))
+
   ;; doom handler
   (set-lookup-handlers! 'nox--managed-mode :async t
     :documentation #'nox-show-doc
