10fec62babc79d46e7f8e14411d8025e08df3765
shixl <xlshiz@users.noreply.github.com>
2021-03-06 23:47:18 +0800

    Sync

--- modules/private/search/packages.el
+++ modules/private/search/packages.el
@@ -4,5 +4,4 @@
 (package! symbol-overlay)
 (package! color-rg :recipe (:host github :repo "manateelazycat/color-rg" :build (autoloads)))
 (when (featurep! +snails)
-  (package! fuz.el :recipe (:host github :repo "cireu/fuz.el" ))
   (package! snails :recipe (:host github :repo "xlshiz/snails" :files ("*.el" "*.sh") :build (autoloads))))
