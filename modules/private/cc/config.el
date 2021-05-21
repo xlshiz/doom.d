9f88da71dd55892fa8f70a213a6a1a82efbdbf6c
shixl <xlshiz@users.noreply.github.com>
2021-05-21 22:05:56 +0800

    Sync

--- modules/private/cc/config.el
+++ modules/private/cc/config.el
@@ -42,5 +42,6 @@
                                 "--background-index"
                                 "--clang-tidy"
                                 "--completion-style=detailed"
-                                "--header-insertion=never"))
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"))
 (after! lsp-clangd (set-lsp-priority! 'clangd 1)) ; ccls has priority 0
